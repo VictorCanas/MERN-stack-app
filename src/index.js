ea3f0b9994d5500e14ba8638d7e3cdab393c002c
VictorCanas <victorcanasint@gmail.com>
2022-10-29 19:20:49 -0500

    Adding comments to index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express(); 
+ 
+// Settings
 
+// Middlewares
+
+// Routes 
+
+// Static Files
+  
+//Starting the server
 app.listen(3000, () => {
   console.log('server on port 3000'); 
-});
+}); 
