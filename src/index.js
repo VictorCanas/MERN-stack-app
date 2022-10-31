01f1c05ab8358c92f342ee2820aa20df818ddffa
VictorCanas <victorcanasint@gmail.com>
2022-10-31 11:07:41 -0500

    Adding routes and html page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { setDefaultResultOrder } = require('dns');
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express(); 
 const port = 3000
 // Settings 
@@ -9,13 +10,19 @@ const port = 3000
 app.use(morgan('dev'));
 //this function checks if is formatted in JSON
 app.use(express.json());
- 
+  
 // Routes
-app.use(require('./routes/task.routes'));
+app.use('/api/tasks' ,require('./routes/task.routes'));
 
 // Static Files
+//Able to show directory of project
+//console.log(path.join(__dirname, 'public'));
 
-//Starting the server
+//Finds the public folder where index.html is being saved and duisplays on localhost:3000/homepage
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Starting the server 
 app.listen(port, () => {
   console.log(`Server on port ${port}`)
 }); 
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,8 +1,12 @@
-const express = require('express');
+//This file defines rest API calls
+
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello Victor!') 
- })
+router.get("/", (req, res) => {
+  res.json({
+    status: 'API works!' //retuns a Javascript object
+  })
+})
 
 module.exports = router;
